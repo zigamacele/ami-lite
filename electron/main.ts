e8688e61c94351d11aefb15d7b476b52dde08b39
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-07-06 21:40:49 +0200

    initial commit

---
+++ electron/main.ts
@@ -0,0 +1,58 @@
+import { app, BrowserWindow } from 'electron'
+import { menubar } from 'menubar'
+import path from 'node:path'
+
+// The built directory structure
+//
+// ├─┬─┬ dist
+// │ │ └── index.html
+// │ │
+// │ ├─┬ dist-electron
+// │ │ ├── main.js
+// │ │ └── preload.js
+// │
+process.env.DIST = path.join(__dirname, '../dist')
+process.env.PUBLIC = app.isPackaged
+  ? process.env.DIST
+  : path.join(process.env.DIST, '../public')
+
+let win: BrowserWindow | null
+// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
+const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+
+function createWindow() {
+  win = new BrowserWindow({
+    icon: path.join(process.env.PUBLIC, 'electron-vite.svg'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+
+  // Test active push message to Renderer-process.
+  win.webContents.on('did-finish-load', () => {
+    win?.webContents.send('main-process-message', new Date().toLocaleString())
+  })
+
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL)
+  } else {
+    // win.loadFile('dist/index.html')
+    win.loadFile(path.join(process.env.DIST, 'index.html'))
+  }
+}
+
+const mb = menubar({
+  icon: path.join(__dirname, '..', 'src', 'assets', 'IconTemplate.png'),
+  index: VITE_DEV_SERVER_URL,
+  browserWindow: { width: 400, height: 600 },
+})
+
+mb.on('ready', () => {
+  console.log('app is ready')
+})
+
+app.on('window-all-closed', () => {
+  win = null
+})
+
+// app.whenReady().then(createWindow);
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className='flex justify-center'>APP</div>
+    </>
+  )
+}
+
+export default App
